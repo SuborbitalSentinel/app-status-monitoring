3fe5f07694859852aa63cd6147f3342b9d1355c3
SuborbitalSentinel <amiller.swe@pm.me>
2023-07-22 18:44:25 -0400

    fixing ls_containers script to return http1.1

--- infrastructure/ls_containers.sh
+++ infrastructure/ls_containers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-while true; do docker ps -a --format '{{.ID}} {{.Names}}' | nc -N -l -p 8000; done
+while true; do echo "HTTP/1.1 200 OK\n\n$(docker ps -a --format '{{.ID}} {{.Names}}')" | nc -N -l -p 8000; done
