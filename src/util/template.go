8b1ba61361ca449163df74004709c04cd69dd95b
SuborbitalSentinel <amiller.swe@pm.me>
2023-07-30 21:48:01 -0400

    Forgot this

---
+++ src/util/template.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type TemplateExecutor interface {
+	Execute(w http.ResponseWriter, data interface{}) error
+}
+
+type DebugTemplateExecutor struct {
+	Filepath string
+}
+
+type ReleaseTemplateExecutor struct {
+	Template *template.Template
+}
+
+func (e DebugTemplateExecutor) Execute(w http.ResponseWriter, data interface{}) error {
+	t := template.Must(template.ParseFiles(e.Filepath))
+
+	return t.Execute(w, data)
+}
+
+func (e ReleaseTemplateExecutor) Execute(w http.ResponseWriter, data interface{}) error {
+	return e.Template.Execute(w, data)
+}
+
